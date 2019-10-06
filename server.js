7fe1468b1d7b83c94c917dd63514a297f9106462
Paul Billings <paulbillings101@yahoo.co.uk>
2019-10-06 18:18:07 +0100

    changed PORT number to environment variable

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.put("/image", (req, res) => { image.handleImage(req, res, db) })
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(3001, () => {
-	console.log("app is running on port 3001");
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
