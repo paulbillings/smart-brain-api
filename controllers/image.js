e1e111dcd72955b4cf9a6e8ae9f129b4cd8e8edd
Paul Billings <paulbillings101@yahoo.co.uk>
2019-10-06 16:18:33 +0100

    moved face recognition api call from the front end plus security tweaks

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '60aee873e4f14469a32c2771e1dd7e0a'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json("Unable to work with API"))
+}
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage: handleImage
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
 }
--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 
 const handleRegister = (req, res, db, bcrypt, saltRounds) => {
 	const { email, name, password } = req.body;
+	if (!email || !name || !password) {
+		return res.status(400).json("Incorrect form submission");
+	}
 	const hash = bcrypt.hashSync(password, saltRounds);
 		db.transaction(trx => {
 			trx.insert({
